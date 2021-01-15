081ae8a16f091623057213749bf080d498efa2d4
ahmet-candan <ahmetcandan1@outlook.com.tr>
2021-01-15 12:15:27 +0300

    bilet alma işlemleri yapılıyor

--- src/satinAl.java
+++ src/satinAl.java
@@ -1,10 +1,16 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class satinAl extends JFrame{
     private JButton button1;
     private JPanel panel9;
+    private JTextField textField1;
+
+    private Connection con = null;
+    private PreparedStatement preparedStatement = null;
 
     public satinAl() {
 
@@ -16,9 +22,17 @@ public class satinAl extends JFrame{
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
                 UserControlPanel a = new UserControlPanel();
                 System.out.println( a.selected);
 
+                String koltuk =textField1.getText();
+
+                String sorgu = "SELECT * FROM terminal where id = ?";
+
+                String databaseUrl = "jdbc:mysql://localhost:3306/bus?useUnicode=true&characterEncoding=utf-8";
+
+                
             }
         });
     }
